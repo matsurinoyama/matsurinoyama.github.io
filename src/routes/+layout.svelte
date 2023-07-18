edf9222d3c72c2db67cc0bd5193c4b283edab73f
Azhan Rizan <matsurinoyama@pm.me>
2023-07-18 16:37:11 +0800

    ✨Created the navbar. Will adjust other settings later.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<nav>
+    <div class="navLinks">
+        <a href="/about"><h5>About</h5></a>
+        <a href="/resume"><h5>Resume</h5></a>
+    </div>
+    <div class="navLinks">
+        <a href="/works"><h5>Works</h5></a>
+        <a href="/contact"><h5>Contact</h5></a>
+    </div>
+    <a href="/"><img src="icon_15-white.png" alt="Home" height="64px"></a>
+</nav>
+
+<slot></slot>
+
+<h6>Copyright (c) 2023 Azhan Rizan / 莉山礼司. All code written for this website are licensed under the MIT License. Read the license for more details.</h6>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let pageTitle = '茉莉の山'
+</script>
+
+<div class="navTitle">
+    <h2 style="letter-spacing: 32px;">{@html pageTitle}</h2>
+</div>
+
+<h1>茉莉の山<br>matsuri<br>noyama</h1>
+<h2>茉莉の山<br>matsuri<br>noyama</h2>
+<h3>茉莉の山<br>matsuri<br>noyama</h3>
+<h4>茉莉の山<br>matsuri<br>noyama</h4>
+<h5>ABOUT ME</h5>
+<p>Wow, a gaijin?</p>
