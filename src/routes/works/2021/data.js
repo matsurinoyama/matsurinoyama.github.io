175162407dfeebc859e926ae43ab54c38784298e
Azhan Rizan / 莉山礼司 <matsurinoyama@proton.me>
2024-02-19 18:03:58 +0800

    🚧Added a temporary landing page

--- src/routes/works/2021/data.js
+++ src/routes/works/2021/data.js
@@ -65,7 +65,7 @@ export const works = [
     title: "Warna-Warni Shah Alam",
     production: "2020年、Dipapansembilan Studio",
     scope:
-      "使用道具・ソフト: Illustrator、外装用上塗り塗料、ブームリフト<br>制作範囲: デザイン、塗料の塗り",
+      "使用道具・ソフト: Illustrator、ブームリフト、<br>　　　　　　　　  塗装刷毛・ローラー等<br>制作範囲: デザイン、塗料の塗り",
     thumbnail: "/2020/WWSA/drawing_01.jpg",
     thumbnail_alt: "drawing_01.jpg",
     content: [
@@ -75,8 +75,12 @@ export const works = [
         Hit & Runとは、DP9 Studio内で開発されたゲームプロジェクトでしたが、残念ながらコンセプト段階から進展することはありませんでした。それにも関わらず、Hit & Runのビジュアル開発に取り組み、私はヒロインのコンセプトアートから3DCGのスカルプトを制作しました。以下のコンセプトアートは、同社の同僚が手がけた作品です。
         </p>
         <div class="mediaContainer_Left">
-          <h6>スカルプトの側面</h6>
-          <img src="/2021/H&R/render_02.png" alt="render_02.png" />
+          <h6>デザインの計画</h6>
+          <img src="/2020/WWSA/drawing_02.jpg" alt="drawing_02.jpg" />
+        </div>
+        <div class="mediaContainer_Landscape">
+          <h6>デザイン計画の根拠</h6>
+          <img src="/2020/WWSA/rationale_01.png" alt="rationale_01.png" />
         </div>
         `,
       },
@@ -85,10 +89,6 @@ export const works = [
         <div class="mediaContainer_Landscape">
           <h6>キャラのコンセプトアート#001<br>作者: <a href="https://twitter.com/legokidrul">@legokidrul</a></h6>
           <img src="/2021/H&R/concept_01.jpg" alt="concept_01.jpg" />
-        </div>
-        <div class="mediaContainer_Landscape">
-          <h6>キャラのコンセプトアート#002<br>作者: <a href="https://twitter.com/legokidrul">@legokidrul</a></h6>
-          <img src="/2021/H&R/concept_02.jpg" alt="concept_02.jpg" />
         </div>
       `,
       },
