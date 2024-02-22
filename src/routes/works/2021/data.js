aac27fd42c6bbadc78cfdd4841cadf42b88cba3f
Azhan Rizan / 莉山礼司 <matsurinoyama@proton.me>
2024-02-22 17:44:01 +0800

    ✅Completed the project page for WWSA

--- src/routes/works/2021/data.js
+++ src/routes/works/2021/data.js
@@ -10,31 +10,40 @@ export const works = [
     content: [
       {
         html: `
-        <p>
-        Hit & Runとは、DP9 Studio内で開発されたゲームプロジェクトでしたが、残念ながらコンセプト段階から進展することはありませんでした。それにも関わらず、Hit & Runのビジュアル開発に取り組み、私はヒロインのコンセプトアートから3DCGのスカルプトを制作しました。以下のコンセプトアートは、同社の同僚が手がけた作品です。
-        </p>
-        <div class="mediaContainer_Left">
-          <h6>スカルプトの側面</h6>
-          <img src="/2021/H&R/render_02.png" alt="render_02.png" />
-        </div>
+          <p>
+            Hit & Run（ヒットエンドラン）は、<a href="https://dp9studio.com">DP9 Studio</a
+            >内で開発されたゲームプロジェクトでしたが、残念ながらコンセプト段階から進展することはありませんでした。それにも関わらず、Hit
+            &
+            Runのビジュアル開発に取り組み、私はヒロインのコンセプトアートから3DCGのスカルプトを制作しました。以下のコンセプトアートは、同社の同僚が手がけた作品です。
+          </p>
+          <div class="mediaContainer_Left">
+            <h6>スカルプトの側面</h6>
+            <img src="/2021/H&R/render_02.png" alt="render_02.png" />
+          </div>
         `,
       },
       {
         html: `
-        <div class="mediaContainer_Landscape">
-          <h6>キャラのコンセプトアート#001<br>作者: <a href="https://twitter.com/legokidrul">@legokidrul</a></h6>
-          <img src="/2021/H&R/concept_01.jpg" alt="concept_01.jpg" />
-        </div>
-        <div class="mediaContainer_Landscape">
-          <h6>キャラのコンセプトアート#002<br>作者: <a href="https://twitter.com/legokidrul">@legokidrul</a></h6>
-          <img src="/2021/H&R/concept_02.jpg" alt="concept_02.jpg" />
-        </div>
+          <div class="mediaContainer_Landscape">
+            <h6>
+              キャラのコンセプトアート#001<br />作者:
+              <a href="https://twitter.com/legokidrul">Zulkarnain Rizan</a>
+            </h6>
+            <img src="/2021/H&R/concept_01.jpg" alt="concept_01.jpg" />
+          </div>
+          <div class="mediaContainer_Landscape">
+            <h6>
+              キャラのコンセプトアート#002<br />作者:
+              <a href="https://twitter.com/legokidrul">Zulkarnain Rizan</a>
+            </h6>
+            <img src="/2021/H&R/concept_02.jpg" alt="concept_02.jpg" />
+          </div>
       `,
       },
       {
         html: `
-        <div class="mediaContainer_Portrait">
-          <h6>3DCGのスカルプト</h6>
+          <div class="mediaContainer_Portrait">
+            <h6>3DCGのスカルプト</h6>
             <iframe
               title="Hit & Run Sculpt Test"
               frameborder="0"
@@ -49,13 +58,13 @@ export const works = [
               src="https://sketchfab.com/models/46e2e2df293d4997a49bbd78c206e6af/embed?autostart=1&camera=0"
             >
             </iframe>
-        </div>
+          </div>
       `,
       },
     ],
     finalimg: "/2021/H&R/blueprint_01.jpg",
     finalimg_alt: "blueprint_01.jpg",
-    finalimg_sub: `スカルプト用のリファレンスシート<br>作者: <a href="https://twitter.com/legokidrul">@legokidrul</a>`,
+    finalimg_sub: `スカルプト用のリファレンスシート<br>作者: <a href="https://twitter.com/legokidrul">Zulkarnain Rizan</a>`,
     finaltxt:
       "リファレンスシートに基づいて、3DCGのスカルプトを制作し、仮のレンダリングやシェーディングなども制作しました。",
   },
@@ -65,46 +74,55 @@ export const works = [
     title: "Warna-Warni Shah Alam",
     production: "2020年、Dipapansembilan Studio",
     scope:
-      "使用道具・ソフト: Illustrator、ブームリフト、<br>　　　　　　　　  塗装刷毛・ローラー等<br>制作範囲: デザイン、塗料の塗り",
+      "使用道具・ソフト: Illustrator、ブームリフト、塗装刷毛・ローラー等<br>制作範囲: デザイン、塗料の塗り",
     thumbnail: "/2020/WWSA/drawing_01.jpg",
     thumbnail_alt: "drawing_01.jpg",
     content: [
       {
         html: `
-        <p>
-        Hit & Runとは、DP9 Studio内で開発されたゲームプロジェクトでしたが、残念ながらコンセプト段階から進展することはありませんでした。それにも関わらず、Hit & Runのビジュアル開発に取り組み、私はヒロインのコンセプトアートから3DCGのスカルプトを制作しました。以下のコンセプトアートは、同社の同僚が手がけた作品です。
-        </p>
-        <div class="mediaContainer_Left">
-          <h6>デザインの計画</h6>
-          <img src="/2020/WWSA/drawing_02.jpg" alt="drawing_02.jpg" />
-        </div>
+          <p>
+            Warna-Warni Shah
+            Alam（ワーナワーニシャーアラム、日本語：シャーアラムの様々な色）は、<a
+              href="https://maps.app.goo.gl/iSDK7kX5ifvKaB7x6"
+              >シャーアラム</a
+            >にある高架下の天井画で、<a href="https://mbsa.gov.my">シャーアラム市役所</a
+            >から依頼されて作りました。私の制作範囲はデザイン提案に限らず、天井画の塗る作業もチームで取り組んでいました。私が取り組んだデザインは、シャーアラムの有名な観光地である<a
+              href="https://ja.wikipedia.org/wiki/%E3%82%B9%E3%83%AB%E3%82%BF%E3%83%B3%E3%83%BB%E3%82%B5%E3%83%A9%E3%83%87%E3%82%A3%E3%83%B3%E3%83%BB%E3%82%A2%E3%83%96%E3%83%89%E3%82%A5%E3%83%AB%E3%83%BB%E3%82%A2%E3%82%B8%E3%82%BA%E3%83%BB%E3%83%A2%E3%82%B9%E3%82%AF"
+              >ブルーモスク</a
+            >に象徴される、イスラム美術系のデザインに着想を得ました。そして、シャーアラム市章に基づくイスラム美術の模様を制作し、Warna-Warni
+            Shah Alamのデザイン提案の根拠となりました。
+          </p>
+          <div class="mediaContainer_Left">
+            <h6>デザイン提案</h6>
+            <img src="/2020/WWSA/drawing_02.jpg" alt="drawing_02.jpg" />
+          </div>
         `,
       },
       {
         html: `
-        <div class="mediaContainer_Landscape">
-          <h6>BEFORE</h6>
-          <img src="/2020/WWSA/before_01.webp" alt="before_01.webp" />
-        </div>
-        <div class="mediaContainer_Landscape">
-          <h6>AFTER</h6>
-          <img src="/2020/WWSA/after_01.webp" alt="after_01.webp" />
-        </div>
+          <div class="mediaContainer_Landscape">
+            <h6>BEFORE</h6>
+            <img src="/2020/WWSA/before_01.webp" alt="before_01.webp" />
+          </div>
+          <div class="mediaContainer_Landscape">
+            <h6>AFTER</h6>
+            <img src="/2020/WWSA/after_01.webp" alt="after_01.webp" />
+          </div>
       `,
       },
       {
         html: `
-        <div class="mediaContainer_Portrait">
-          <h6>デザイン計画の根拠</h6>
-          <img src="/2020/WWSA/rationale_01.webp" alt="rationale_01.webp" />
-        </div>
+          <div class="mediaContainer_Portrait">
+            <h6>デザイン提案の根拠</h6>
+            <img src="/2020/WWSA/rationale_01.gif" alt="rationale_01.gif" />
+          </div>
       `,
       },
     ],
-    finalimg: "/2021/H&R/blueprint_01.jpg",
-    finalimg_alt: "blueprint_01.jpg",
-    finalimg_sub: `スカルプト用のリファレンスシート<br>作者: <a href="https://twitter.com/legokidrul">@legokidrul</a>`,
+    finalimg: "/2020/WWSA/inspiration_01.jpg",
+    finalimg_alt: "inspiration_01.jpg",
+    finalimg_sub: `デザイン提案のインスピレーション<br>作者: <a href="https://instagram.com/ronmuralist">Martín Ron</a>`,
     finaltxt:
-      "リファレンスシートに基づいて、3DCGのスカルプトを制作し、仮のレンダリングやシェーディングなども制作しました。",
+      "さらに、ブエノスアイレスにあるMartín Ronの天井画に最終のデザインとして、インスピレーションを得ました。",
   },
 ];
